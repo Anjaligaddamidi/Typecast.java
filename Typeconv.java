f8e0ad4e602b6ebe12f342bdaf75ddf224e9a348
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-03-21 11:45:33 +0530

    Add files via upload

---
+++ Typeconv.java
@@ -0,0 +1,8 @@
+public class Typeconv{
+    public static void main(String[] args){
+        int rno=125;
+        double rnn=rno;
+        System.out.println(rno);
+        System.out.println(rnn);
+    }
+}
